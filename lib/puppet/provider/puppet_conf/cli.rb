98041497fee897d7b4997c99aaf608c0fd167da9
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-07-10 10:18:35 +0200

    Add self.instances and self.prefetch in cli provider

--- lib/puppet/provider/puppet_conf/cli.rb
+++ lib/puppet/provider/puppet_conf/cli.rb
@@ -2,10 +2,27 @@ Puppet::Type.type(:puppet_conf).provide(:cli) do
 
   mk_resource_methods
 
+  commands :puppet => 'puppet'
+
   def self.instances
+    puppet('master', '--configprint', 'all').split("\n").collect do |line|
+      key, value = line.split(' = ')
+      new({
+        :name   => "master/#{key}",
+        :ensure => :present,
+        :key    => key,
+        :value  => value,
+      })
+    end
   end
 
   def self.prefetch
+    items = instances
+    resources.keys.each do |name|
+      if provider = items.find{ |pkg| pkg.name == name }
+        resources[name].provider = provider
+      end
+    end
   end
 
   def exists?
--- spec/unit/puppet/provider/puppet_conf/cli_spec.rb
+++ spec/unit/puppet/provider/puppet_conf/cli_spec.rb
@@ -15,6 +15,19 @@ describe Puppet::Type.type(:puppet_conf).provider(:cli) do
         it 'should have an instance method' do
           expect(described_class).to respond_to :instances
         end
+        context 'when validating instances' do
+          before :each do
+            described_class.expects(:puppet).with('master', '--configprint', 'all').returns \
+'agent_catalog_run_lockfile = /var/lib/puppet/state/agent_catalog_run.lock
+agent_disabled_lockfile = /var/lib/puppet/state/agent_disabled.lock
+allow_duplicate_certs = false
+allow_variables_with_dashes = false
+'
+          end
+          it 'should return no resources' do
+            expect(described_class.instances.size).to eq(4)
+          end
+        end
       end
 
       describe 'prefetch' do
